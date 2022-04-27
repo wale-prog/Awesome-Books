9d71fa2e127c2b669c0305ff728077bdbd0c82bb
ahzamir <ahmadzamiryousofi1397@gmail.com>
2022-04-27 22:51:34 +0430

    add the class functions to js

---
+++ Books.js
@@ -0,0 +1,20 @@
+export default class Books {
+  constructor(books) {
+    this.books = books || [];
+  }
+  add = (title, author) => {
+    this.books.push({
+      title,
+      author,
+    });
+  }
+  remove = (title) => {
+    this.books = this.books.filter((book) => {
+      const newTitleFromArray = book.title.replace(/\s+/g, '');
+      return (newTitleFromArray !== title);
+    });
+  }
+  getAll = () => {
+    return this.books;
+  }
+};
