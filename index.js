b67801192cd6159f3b5c59ffa6609acc8db3b1bf
ahzamir <ahmadzamiryousofi1397@gmail.com>
2022-04-27 05:23:11 +0430

    solving linters error

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ const awesomeBooks = document.getElementById('awesome-books');
 const booksContainer = document.querySelector('.new-books');
 const titleInput = document.querySelector('.title-input');
 const authorInput = document.querySelector('.author-input');
-let books = JSON.parse(localStorage.getItem("books")) || [];
+let books = JSON.parse(localStorage.getItem('books')) || [];
 const addBooks = (title, author) => {
   books.push({
     title,
-    author
+    author,
   });
 
-  localStorage.setItem("books", JSON.stringify(books))
-  return { title, author }
+  localStorage.setItem('books', JSON.stringify(books));
+  return { title, author };
 };
 
 const createNewBook = ({ title, author }) => {
@@ -19,52 +19,46 @@ const createNewBook = ({ title, author }) => {
   const newBookAuthor = document.createElement('p');
   const button = document.createElement('button');
   const line = document.createElement('hr');
-  let titleNoSpace = title.replace(/\s+/g, '');//take the title and remove space//
-  let removeId = 'remove' + titleNoSpace;//removeId add the remove on titleNoSpace//
-  let divId = 'div' + titleNoSpace;//divId add the div to titleNoSpace//
+  const titleNoSpace = title.replace(/\s+/g, '');
+  const removeId = `remove${titleNoSpace}`;
+  const divId = `div${titleNoSpace}`;
 
-  newBooksDiv.setAttribute('id', divId);//the div id set to div+title//
+  newBooksDiv.setAttribute('id', divId);
   button.innerText = 'Remove';
   button.className = 'remove-btn';
-  button.setAttribute('id', removeId);//the remove button id set to remove+title//
+  button.setAttribute('id', removeId);
   newBookTitle.innerText = title;
   newBookAuthor.innerText = author;
 
   newBooksDiv.append(newBookTitle, newBookAuthor, button, line);
   booksContainer.appendChild(newBooksDiv);
-}
+};
 
 books.forEach(createNewBook);
 
 awesomeBooks.onsubmit = (event) => {
   event.preventDefault();
 
-  const newBookEntry = addBooks(titleInput.value, authorInput.value
-  );
+  const newBookEntry = addBooks(titleInput.value, authorInput.value);
 
-  createNewBook(newBookEntry)
-  titleInput.value = ""
-  authorInput.value = ""
+  createNewBook(newBookEntry);
+  titleInput.value = '';
+  authorInput.value = '';
 };
-const removeContenet = (event) => {
-  let eventId = event.target.id;
+const booksListAction = (event) => {
+  const eventId = event.target.id;
   if (eventId.includes('remove')) {
-    //remove "remove" to find the name without space
-    let name = eventId = eventId.replace('remove', '');
-    //create divid
-    let divId = eventId = 'div' + eventId;
-    //remove the element from array , filter, for each element ,remove space compare with name        //
+    const bookTitle = eventId.replace('remove', '');
+    const divId = `div${eventId}`;
     books = books.filter((book) => {
-      let nameFromArray = book.title.replace(/\s+/g, '');
-      return (nameFromArray !== name);
-    })
-    //update local storage based on new array
-    localStorage.setItem("books", JSON.stringify(books));
-    //remove element divid
+      const newTitleFromArray = book.title.replace(/\s+/g, '');
+      return (newTitleFromArray !== bookTitle);
+    });
+    localStorage.setItem('books', JSON.stringify(books));
     document.getElementById(divId).remove();
   }
-}
+};
 
 window.addEventListener('DOMContentLoaded', () => {
-  document.getElementById('top-books').addEventListener('click', removeContenet);
-})
+  document.getElementById('top-books').addEventListener('click', booksListAction);
+});
