21620eb1e8ddb09aa4673ebbf03155bfb53f49af
wale-prog <olapetanwale@outlook.com>
2022-04-28 11:52:35 +0100

    Implement book list and add book form

--- index.js
+++ index.js
@@ -48,20 +48,57 @@ window.addEventListener('DOMContentLoaded', () => {
   displayBooks(library.getAll());
 });
 
+
+// const navContent = `<p>Awesome Books</p>
+// <ul class="nav-list">
+//   <li class="nav-list-item"><a href="#">List</a></li>
+//   <li class="nav-list-item"><a href="#">Add new</a></li>
+//   <li class="nav-list-item"><a href="#">Contact</a></li>
+// </ul>
+// `;
+// const navbarContent = document.createElement('div');
+// navbarContent.setAttribute("id", "navbar")
+// const navbar = document.getElementById('navbar')
+// const navbarhead = document.querySelector('.navbar')
+// navbarContent.innerHTML = navContent;
+// navbarhead.appendChild(navbarContent);
+
 const navList = document.querySelectorAll('.nav-list-item');
+
+
+const dateDisplay = document.querySelector('.date-display');
+const datePara = document.createElement('p')
+
+function manageDate(datePara) {
+  const date = new Date();
+  const myDay = ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat'];
+  let dayOfWeek = date.getDay();
+  let dayOfMonth = date.getDate();
+  let year = date.getFullYear();
+  let hour = date.getHours();
+  let minutes = date.getMinutes();
+  let innerHTMLP = `${myDay[dayOfWeek]} Apr ${dayOfMonth}, ${year} ${hour}:${minutes}`;
+  datePara.innerHTML = innerHTMLP;
+  dateDisplay.appendChild(datePara);
+}
+manageDate(datePara);
+
 const list = document.querySelector('.list');
-const addNew = document.querySelector('.add-new')
+const addNew = document.querySelector('.add-new');
+
 
 for (let i = 0; i < navList.length; i += 1) {
   navList[i].addEventListener('click', (event) => {
     if (event.target.innerText === `Add new`) {
       list.classList.add('hidden');
-      addNew.classList.remove('hidden')
+      addNew.classList.remove('hidden');
+      contact.classList.add('hidden')
     } else if (event.target.innerText === "List") {
       list.classList.remove('hidden');
       addNew.classList.add('hidden')
+      contact.classList.add('hidden')
     }
-
+    manageDate(datePara);
   })
 }
 
